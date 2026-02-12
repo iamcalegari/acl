44f93e4edbcc6b77815a3b6b6cebb382bda609aa
iamcalegari <iamcalegari20@gmail.com>
2026-02-12 12:26:09 -0300

    wip

--- libs/plugins/default-public.ts
+++ libs/plugins/default-public.ts
@@ -5,8 +5,6 @@ import { setGuardsRoute } from "./helpers/route-visibility.helpers";
 
 
 export const defaultPublicPlugin: FastifyPluginAsync = fp(async (app, { force }: { force?: boolean } = {}) => {
-
-
   app.addHook("onRoute", (route: RouteOptions) => {
     const cfg = (route.config ?? {}) as any;
     route.config = { ...cfg, guards: new Set<GuardName>([]) };
